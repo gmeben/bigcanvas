624e056c9613001fb118c40d6b56d66254f5916f
gmeben <gmeben@gmail.com>
2020-08-05 09:48:53 -0400

    feat: Add jQuery 3.5.1 to PHP version

--- php/src/draw.php
+++ php/src/draw.php
@@ -8,6 +8,7 @@
 <body>
     <h1><a href="/">bigcanvas PHP</a> | draw</h1>
     <canvas id="canvas" width="500" height="500" style="border: 1px solid #000"></canvas>
+    <script src="jquery-3.5.1.js"></script>
     <script src="draw.js"></script>
 </body>
 </html>
